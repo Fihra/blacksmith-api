cfed62c5917b22ecf2b29367fb69811dfd92ac63
Fabian Fabro <fabrofabian@gmail.com>
2019-12-04 20:31:54 -0800

    yay

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//Middlewares
+
+//Import Routes
+
+//DB Connection
